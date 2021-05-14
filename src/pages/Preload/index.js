2c3cb13965fa740594810ae0f80d781f00cb321d
Gabriel Vieira <gsvieira98@gmail.com>
2021-05-14 02:17:37 -0300

    add signUp and signIn fix backend

--- src/pages/Preload/index.js
+++ src/pages/Preload/index.js
@@ -1,21 +1,40 @@
 import React, { useEffect } from 'react';
-import { Text } from 'react-native';
 import { Container, LoadingIcon } from './styles';
 import AsyncStorage from '@react-native-community/async-storage'
 import { useNavigation } from '@react-navigation/native';
+
+import { UserContext } from '../../contexts/UserContext';
+import Api from '~/services/api';
 
 import CalendarLogo from '~/assets/logo_1.svg'
 
 
 export default () => {
 
+    const { dispatch: userDispatch } = useContext(UserContext);
     const navigation = useNavigation();
 
     useEffect(() => {
         const checkToken = async () =>{
             const token = await AsyncStorage.getItem('token');
             if(token){
-                //validar token
+                let res = await Api.checkToken(token);
+                if (res.token) {
+                    await AsyncStorage.setItem('token', json.token);
+
+                    userDispatch({
+                        type: 'setAvatar',
+                        payload:{
+                            avatar: json.data.avatar
+                        }
+                    });
+
+                    navigation.reset({
+                        routes:[{name:'MainTab'}]
+                    });
+            } else {
+                    navigation.navigate('SignIn')
+                }
             }else{
                 navigation.navigate('SignIn')
             }
