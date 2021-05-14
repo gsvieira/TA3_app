d1391a88e796895a3b3f14afb0907d512fb7a3f0
Gabriel Vieira <gsvieira98@gmail.com>
2021-05-14 02:43:56 -0300

    add new pages and custom tab bar NeedToFixBackEnd

---
+++ src/pages/Calendar/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { Text } from 'react-native'
+import { Container, LoadingIcon } from './styles';
+import AsyncStorage from '@react-native-community/async-storage'
+import { useNavigation } from '@react-navigation/native';
+
+export default () => {
+    return (
+        <Container>
+            <Text>calendar</Text>
+        </Container>
+    )
+}
--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -3,10 +3,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-import Preload from '../pages/Preload';
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
-
+import Preload from '~/pages/Preload';
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+import MainTab from '~/pages/MainTab';
 
 export default () => (
     <Stack.Navigator
@@ -18,6 +18,7 @@ export default () => (
         <Stack.Screen name="Preload" component={Preload} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="MainTab" component={SignUp} />
     </Stack.Navigator>
 )
 
